29f0672a149a5caa1dd517f6ae29375dee132f92
tahmidefaz <tahmidefaz@gmail.com>
2022-03-02 15:51:01 -0500

    changing api endpoint

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5001',
+        target: 'https://hourdle-backend.herokuapp.com',
         changeOrigin: true,
         secure: false,
       },
